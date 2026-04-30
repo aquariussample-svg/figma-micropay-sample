5da739ff7b78d5665a00a5934a48b241826d8f27
aquariussample-svg <aquariussample@gmail.com>
2026-04-30 11:30:15 +0530

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,6 @@
-import { Navigate, createBrowserRouter, isRouteErrorResponse, useRouteError } from "react-router";
+import { createBrowserRouter, isRouteErrorResponse, useRouteError } from "react-router";
 import { Layout } from "./components/Layout";
+import { Login } from "./pages/Login";
 import { Dashboard } from "./pages/Dashboard";
 import { Employees } from "./pages/Employees";
 import { Payroll } from "./pages/Payroll";
@@ -36,7 +37,7 @@ function RouteErrorPage() {
 }
 
 export const router = createBrowserRouter([
-  { path: "/login", element: <Navigate to="/dashboard" replace />, errorElement: <RouteErrorPage /> },
+  { path: "/login", Component: Login, errorElement: <RouteErrorPage /> },
   {
     path: "/",
     Component: Layout,
